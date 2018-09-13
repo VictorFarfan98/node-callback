9245995cd528cdfbb1dab238ebaec68e2509b494
Victor Farfan <vfarfan@ufm.edu>
2018-09-13 08:13:18 -0600

    Corregido hacia la funcion Callback

--- readAsyncCb.js
+++ readAsyncCb.js
@@ -4,10 +4,17 @@ function callback  (err, data) {
     if (err) return console.error(err);
     var textPlain = data.toString()
     console.log(textPlain)
+
+    var myArray = textPlain.split("\n")
+    myArray.forEach(function(value){
+        //console.log(value.split(" ").length);
+        fs.appendFileSync('./writeTest', value.split(" ").length +'\n');
+    });
 };
 function readFile(){
     //fs.readFile("./fileTest", function (err, data) {
-    var buffer = fs.readFile("./fileTest",callback )
-  
+    var buffer = fs.readFile("./fileTest",callback )   
 }
+
+
 readFile()
